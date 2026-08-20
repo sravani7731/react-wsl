7abd0a8b6bfd3674a640b182cb73d6d65571e830
sravani7731 <madugulasravani18@gmail.com>
2026-08-20 13:18:29 +0530

    Integrate React component into test.py
    Added a simple React component to the Python application file.

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 # it is a python application file 
+function App() {
+  return (
+    <div>
+      <h1>Hello, Welcome to React!</h1>
+      <p>This is my first React application.</p>
+    </div>
+  );
+}
+
+export default App;
